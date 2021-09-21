7f074396852d9e1265de9f9ae8f4a1f73918c39e
iisydnii <61031342+iisydnii@users.noreply.github.com>
2021-09-20 20:30:27 -0400

    Update to SituationCard

---
+++ ScrumAge/SituationCard.cs
@@ -0,0 +1,30 @@
+﻿/*-------------
+Name: Sydni Ward
+Date created: 09/20/21
+File name: SituationCard
+Purpose: Template for creating a situation card
+-------------*/
+using System;
+
+namespace ScrumAge
+{
+    public class SituationCard
+    {
+        /* Create rules for the inputs*/
+        public int id { get; set; }
+        public string situation { get; set; }
+        public Array cost { get; set; }
+        public Array reward { get; set; }
+        public int used { get; set; }
+
+        public SituationCard(int id, string situation, Array cost,
+            Array reward, int used)
+        {
+            this.id = id;
+            this.situation = situation;
+            this.cost = cost;
+            this.reward = reward;
+            this.used = used;
+        }
+    }
+}
