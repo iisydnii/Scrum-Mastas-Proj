092ed855058f5468d3ce92f105ac7f53ca8d309b
floydrp <70908848+floydrp@users.noreply.github.com>
2021-10-01 18:33:06 -0400

    Adding dice class

---
+++ ScrumAge/Dice.cs
@@ -0,0 +1,50 @@
+﻿/*-------------
+Name: ScrumAge
+Author: Piper Floyd 
+Date created: 09/20/21
+File name: Dice
+Purpose: Acts as a dice roll. Returns a numeric total of the rolled dice
+-------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ScrumAge
+{
+    class Dice
+    {
+        Random random = new Random();
+
+
+        /// <summary>
+        /// This method rolls a dice and adds the values of the dice together. 
+        /// </summary>
+        /// <returns>A total of all the dice rolls.</returns>
+        public int RollDice()
+        {
+            var diceValue = 0;
+            var diceTotal = 0;
+
+
+            for (int i = 0; i < 2; i++)
+            {
+                diceValue = random.Next(1, 6);
+                diceTotal += diceValue;
+            }
+
+
+            //Discerns snake eyes
+            //Snake eyes returns a 0
+            if (diceTotal == 2)
+            {
+                diceTotal = 0;
+            }
+
+
+            return diceTotal;
+        }
+
+    }
+}
