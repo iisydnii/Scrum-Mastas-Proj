106a27df5df4965c2cc1b831177bc06ed184ccfb
bacenmusic <busicm@etsu.edu>
2021-10-02 13:05:57 -0400

    Created classes for Player
    Created Inventory, Certifications, and Player classes. Create form that lets the user select a number of players and assign them names.

---
+++ ScrumAge/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrumAge
+{
+    class Player
+    {
+/*-------------
+Name: Macen Busic
+Date created: 09/22/21
+File name: Player.cs
+Purpose: Create a player
+-------------*/
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Inventory Inventory { get; set; }
+
+        public Player(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Inventory = new Inventory();
+        }
+
+        public Player(int id)
+        {
+            this.Id = id;
+            this.Name = "";
+            this.Inventory = new Inventory();
+        }
+    }
+}
--- ScrumAge/Program.cs
+++ ScrumAge/Program.cs
@@ -22,7 +22,7 @@ namespace ScrumAge
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SelectPlayersForm());
 
 
             ///Test
