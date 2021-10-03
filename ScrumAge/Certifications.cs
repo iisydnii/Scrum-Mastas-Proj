1dcee1ab30ce0ff4eb3a2deaa63a6947e2b5fb1f
Sydni Ward <61031342+iisydnii@users.noreply.github.com>
2021-10-03 17:50:14 -0400

    Fixing conflicts part one

---
+++ ScrumAge/Certifications.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrumAge
+{
+    public class Certifications
+    {
+/*-------------
+Name: Macen Busic
+Date created: 10/01/21
+File name: Certifications.cs
+Purpose: Create and update
+Players list of certifications
+-------------*/
+        public string Certification { get; set; }
+        public List<string> CertificationsList { get; set; }
+
+        public Certifications()
+        {
+            CertificationsList = new List<string>();
+        }
+
+        public void AddCertification(string certification)
+        {
+            CertificationsList.Add(certification);
+        }
+    }
+}
---
+++ ScrumAge/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrumAge
+{
+/*-------------
+Name: Macen Busic
+Date created: 10/01/21
+File name: Inventory.cs
+Purpose: Create and update
+Player Inventory
+-------------*/
+    public class Inventory
+    {
+        public int Bitcoin { get; set; }
+        public int TrainingPoints { get; set; }
+        public int DevelopmentPoints { get; set; }
+        public int DesignPoints { get; set; }
+        public static int Developers { get; set; }
+        public Certifications Certifications { get; set; }
+       
+        public Inventory()
+        {
+            this.Bitcoin = 0;
+            this.TrainingPoints = 12;
+            this.DevelopmentPoints = 0;
+            this.DesignPoints = 0;
+            Developers = 3;
+            Certifications = null;
+        }
+    }
+}
