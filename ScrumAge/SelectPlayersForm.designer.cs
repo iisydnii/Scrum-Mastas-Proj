47b8337190b93ca6c1c2728df30af83201831ca4
Sydni Ward <61031342+iisydnii@users.noreply.github.com>
2021-10-08 17:04:42 -0400

    Updating Color and font

--- ScrumAge/SelectPlayersForm.designer.cs
+++ ScrumAge/SelectPlayersForm.designer.cs
@@ -51,12 +51,12 @@ namespace ScrumAge
             // Begin
             // 
             this.Begin.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.Begin.BackColor = System.Drawing.Color.Transparent;
-            this.Begin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Begin.Location = new System.Drawing.Point(250, 329);
-            this.Begin.Margin = new System.Windows.Forms.Padding(2);
+            this.Begin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(124)))), ((int)(((byte)(116)))));
+            this.Begin.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold);
+            this.Begin.Location = new System.Drawing.Point(337, 348);
+            this.Begin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Begin.Name = "Begin";
-            this.Begin.Size = new System.Drawing.Size(100, 25);
+            this.Begin.Size = new System.Drawing.Size(101, 32);
             this.Begin.TabIndex = 0;
             this.Begin.Text = "Begin";
             this.Begin.UseVisualStyleBackColor = false;
@@ -66,8 +66,8 @@ namespace ScrumAge
             // label1
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.Font = new System.Drawing.Font("Consolas", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.label1.Location = new System.Drawing.Point(88, 20);
+            this.label1.Font = new System.Drawing.Font("Stencil", 16.75F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(177, 64);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(426, 40);
@@ -78,8 +78,8 @@ namespace ScrumAge
             // label2
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.Font = new System.Drawing.Font("Consolas", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.label2.Location = new System.Drawing.Point(172, 60);
+            this.label2.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(261, 105);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(256, 40);
             this.label2.TabIndex = 2;
@@ -90,10 +90,10 @@ namespace ScrumAge
             // 
             this.Player1Label.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Player1Label.AutoSize = true;
-            this.Player1Label.Font = new System.Drawing.Font("Consolas", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.Player1Label.Location = new System.Drawing.Point(174, 152);
+            this.Player1Label.Font = new System.Drawing.Font("Stencil", 15F, System.Drawing.FontStyle.Bold);
+            this.Player1Label.Location = new System.Drawing.Point(263, 183);
             this.Player1Label.Name = "Player1Label";
-            this.Player1Label.Size = new System.Drawing.Size(120, 23);
+            this.Player1Label.Size = new System.Drawing.Size(121, 24);
             this.Player1Label.TabIndex = 3;
             this.Player1Label.Text = "Player 1: ";
             // 
@@ -101,10 +101,10 @@ namespace ScrumAge
             // 
             this.Player2Label.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Player2Label.AutoSize = true;
-            this.Player2Label.Font = new System.Drawing.Font("Consolas", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.Player2Label.Location = new System.Drawing.Point(174, 196);
+            this.Player2Label.Font = new System.Drawing.Font("Stencil", 15F, System.Drawing.FontStyle.Bold);
+            this.Player2Label.Location = new System.Drawing.Point(263, 227);
             this.Player2Label.Name = "Player2Label";
-            this.Player2Label.Size = new System.Drawing.Size(109, 23);
+            this.Player2Label.Size = new System.Drawing.Size(115, 24);
             this.Player2Label.TabIndex = 4;
             this.Player2Label.Text = "Player 2:";
             // 
@@ -112,10 +112,10 @@ namespace ScrumAge
             // 
             this.Player3Label.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Player3Label.AutoSize = true;
-            this.Player3Label.Font = new System.Drawing.Font("Consolas", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.Player3Label.Location = new System.Drawing.Point(174, 234);
+            this.Player3Label.Font = new System.Drawing.Font("Stencil", 15F, System.Drawing.FontStyle.Bold);
+            this.Player3Label.Location = new System.Drawing.Point(263, 265);
             this.Player3Label.Name = "Player3Label";
-            this.Player3Label.Size = new System.Drawing.Size(109, 23);
+            this.Player3Label.Size = new System.Drawing.Size(115, 24);
             this.Player3Label.TabIndex = 5;
             this.Player3Label.Text = "Player 3:";
             // 
@@ -123,17 +123,17 @@ namespace ScrumAge
             // 
             this.Player4Label.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Player4Label.AutoSize = true;
-            this.Player4Label.Font = new System.Drawing.Font("Consolas", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.Player4Label.Location = new System.Drawing.Point(174, 275);
+            this.Player4Label.Font = new System.Drawing.Font("Stencil", 15F, System.Drawing.FontStyle.Bold);
+            this.Player4Label.Location = new System.Drawing.Point(263, 307);
             this.Player4Label.Name = "Player4Label";
-            this.Player4Label.Size = new System.Drawing.Size(109, 23);
+            this.Player4Label.Size = new System.Drawing.Size(115, 24);
             this.Player4Label.TabIndex = 6;
             this.Player4Label.Text = "Player 4:";
             // 
             // Player1TextBox
             // 
             this.Player1TextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.Player1TextBox.Location = new System.Drawing.Point(294, 155);
+            this.Player1TextBox.Location = new System.Drawing.Point(383, 186);
             this.Player1TextBox.Name = "Player1TextBox";
             this.Player1TextBox.Size = new System.Drawing.Size(134, 20);
             this.Player1TextBox.TabIndex = 8;
@@ -141,7 +141,7 @@ namespace ScrumAge
             // Player2Textbox
             // 
             this.Player2Textbox.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.Player2Textbox.Location = new System.Drawing.Point(294, 196);
+            this.Player2Textbox.Location = new System.Drawing.Point(383, 227);
             this.Player2Textbox.Name = "Player2Textbox";
             this.Player2Textbox.Size = new System.Drawing.Size(134, 20);
             this.Player2Textbox.TabIndex = 9;
@@ -149,7 +149,7 @@ namespace ScrumAge
             // Player3Textbox
             // 
             this.Player3Textbox.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.Player3Textbox.Location = new System.Drawing.Point(294, 237);
+            this.Player3Textbox.Location = new System.Drawing.Point(383, 268);
             this.Player3Textbox.Name = "Player3Textbox";
             this.Player3Textbox.Size = new System.Drawing.Size(134, 20);
             this.Player3Textbox.TabIndex = 10;
@@ -157,7 +157,7 @@ namespace ScrumAge
             // Player4Textbox
             // 
             this.Player4Textbox.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.Player4Textbox.Location = new System.Drawing.Point(294, 278);
+            this.Player4Textbox.Location = new System.Drawing.Point(383, 309);
             this.Player4Textbox.Name = "Player4Textbox";
             this.Player4Textbox.Size = new System.Drawing.Size(134, 20);
             this.Player4Textbox.TabIndex = 11;
@@ -167,7 +167,7 @@ namespace ScrumAge
             this.NumOfPlayersDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.NumOfPlayersDropDown.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.NumOfPlayersDropDown.FormattingEnabled = true;
-            this.NumOfPlayersDropDown.Location = new System.Drawing.Point(229, 113);
+            this.NumOfPlayersDropDown.Location = new System.Drawing.Point(328, 148);
             this.NumOfPlayersDropDown.Name = "NumOfPlayersDropDown";
             this.NumOfPlayersDropDown.Size = new System.Drawing.Size(121, 21);
             this.NumOfPlayersDropDown.TabIndex = 12;
@@ -177,7 +177,8 @@ namespace ScrumAge
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(235)))), ((int)(((byte)(208)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.NumOfPlayersDropDown);
             this.Controls.Add(this.Player4Textbox);
             this.Controls.Add(this.Player3Textbox);
@@ -190,9 +191,10 @@ namespace ScrumAge
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Begin);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SelectPlayersForm";
-            this.Text = "Welcome! Select Players";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Select Players";
             this.Load += new System.EventHandler(this.SelectPlayersForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
