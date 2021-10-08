6578de826ae01a94ae130a84fa7b53cb1fb97dca
Sydni Ward <61031342+iisydnii@users.noreply.github.com>
2021-10-08 17:31:04 -0400

    save

--- ScrumAge/Placement_Board.Designer.cs
+++ ScrumAge/Placement_Board.Designer.cs
@@ -29,73 +29,182 @@ namespace ScrumAge
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Placement_Board));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.Recruiting = new System.Windows.Forms.ComboBox();
-            this.BootCamp = new System.Windows.Forms.ComboBox();
-            this.WhiteBoard = new System.Windows.Forms.ComboBox();
-            this.CryptoMarket = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.StatusBox = new System.Windows.Forms.GroupBox();
+            this.holdDevelopers = new System.Windows.Forms.PictureBox();
+            this.Developers = new System.Windows.Forms.PictureBox();
+            this.ProjectTiles = new System.Windows.Forms.GroupBox();
+            this.HRBox = new System.Windows.Forms.PictureBox();
+            this.BootCampBox = new System.Windows.Forms.PictureBox();
+            this.WhiteBoardBox = new System.Windows.Forms.PictureBox();
+            this.BitcoinMarketBox = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.StatusBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.holdDevelopers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Developers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HRBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BootCampBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WhiteBoardBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BitcoinMarketBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(101, 22);
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(626, 159);
-            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.Size = new System.Drawing.Size(235, 213);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // Recruiting
+            // pictureBox2
             // 
-            this.Recruiting.FormattingEnabled = true;
-            this.Recruiting.Location = new System.Drawing.Point(116, 187);
-            this.Recruiting.Name = "Recruiting";
-            this.Recruiting.Size = new System.Drawing.Size(121, 21);
-            this.Recruiting.TabIndex = 11;
-            this.Recruiting.SelectedIndexChanged += new System.EventHandler(this.Recruiting_SelectedIndexChanged);
+            this.pictureBox2.ErrorImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(594, -14);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(166, 199);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
             // 
-            // BootCamp
+            // pictureBox3
             // 
-            this.BootCamp.FormattingEnabled = true;
-            this.BootCamp.Location = new System.Drawing.Point(272, 187);
-            this.BootCamp.Name = "BootCamp";
-            this.BootCamp.Size = new System.Drawing.Size(121, 21);
-            this.BootCamp.TabIndex = 12;
-            this.BootCamp.SelectedIndexChanged += new System.EventHandler(this.BootCamp_SelectedIndexChanged);
+            this.pictureBox3.Location = new System.Drawing.Point(438, 29);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(110, 146);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
             // 
-            // WhiteBoard
+            // pictureBox4
             // 
-            this.WhiteBoard.FormattingEnabled = true;
-            this.WhiteBoard.Location = new System.Drawing.Point(432, 187);
-            this.WhiteBoard.Name = "WhiteBoard";
-            this.WhiteBoard.Size = new System.Drawing.Size(121, 21);
-            this.WhiteBoard.TabIndex = 13;
-            this.WhiteBoard.SelectedIndexChanged += new System.EventHandler(this.WhiteBoard_SelectedIndexChanged);
+            this.pictureBox4.Location = new System.Drawing.Point(249, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(111, 172);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
             // 
-            // CryptoMarket
+            // StatusBox
             // 
-            this.CryptoMarket.FormattingEnabled = true;
-            this.CryptoMarket.Location = new System.Drawing.Point(590, 187);
-            this.CryptoMarket.Name = "CryptoMarket";
-            this.CryptoMarket.Size = new System.Drawing.Size(121, 21);
-            this.CryptoMarket.TabIndex = 14;
-            this.CryptoMarket.SelectedIndexChanged += new System.EventHandler(this.CryptoMarket_SelectedIndexChanged);
+            this.StatusBox.Controls.Add(this.holdDevelopers);
+            this.StatusBox.Location = new System.Drawing.Point(7, 262);
+            this.StatusBox.Name = "StatusBox";
+            this.StatusBox.Size = new System.Drawing.Size(388, 184);
+            this.StatusBox.TabIndex = 4;
+            this.StatusBox.TabStop = false;
+            this.StatusBox.Text = "StatusBox";
+            // 
+            // holdDevelopers
+            // 
+            this.holdDevelopers.Location = new System.Drawing.Point(312, 49);
+            this.holdDevelopers.Name = "holdDevelopers";
+            this.holdDevelopers.Size = new System.Drawing.Size(50, 85);
+            this.holdDevelopers.TabIndex = 0;
+            this.holdDevelopers.TabStop = false;
+            // 
+            // Developers
+            // 
+            this.Developers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Developers.Location = new System.Drawing.Point(322, 33);
+            this.Developers.Name = "Developers";
+            this.Developers.Size = new System.Drawing.Size(50, 84);
+            this.Developers.TabIndex = 0;
+            this.Developers.TabStop = false;
+            // 
+            // ProjectTiles
+            // 
+            this.ProjectTiles.Location = new System.Drawing.Point(401, 262);
+            this.ProjectTiles.Name = "ProjectTiles";
+            this.ProjectTiles.Size = new System.Drawing.Size(376, 184);
+            this.ProjectTiles.TabIndex = 5;
+            this.ProjectTiles.TabStop = false;
+            this.ProjectTiles.Text = "ProjectTiles";
+            // 
+            // HRBox
+            // 
+            this.HRBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.HRBox.Location = new System.Drawing.Point(42, 172);
+            this.HRBox.Name = "HRBox";
+            this.HRBox.Size = new System.Drawing.Size(130, 84);
+            this.HRBox.TabIndex = 6;
+            this.HRBox.TabStop = false;
+            // 
+            // BootCampBox
+            // 
+            this.BootCampBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BootCampBox.Location = new System.Drawing.Point(239, 172);
+            this.BootCampBox.Name = "BootCampBox";
+            this.BootCampBox.Size = new System.Drawing.Size(130, 84);
+            this.BootCampBox.TabIndex = 7;
+            this.BootCampBox.TabStop = false;
+            // 
+            // WhiteBoardBox
+            // 
+            this.WhiteBoardBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.WhiteBoardBox.Location = new System.Drawing.Point(428, 172);
+            this.WhiteBoardBox.Name = "WhiteBoardBox";
+            this.WhiteBoardBox.Size = new System.Drawing.Size(130, 84);
+            this.WhiteBoardBox.TabIndex = 8;
+            this.WhiteBoardBox.TabStop = false;
+            // 
+            // BitcoinMarketBox
+            // 
+            this.BitcoinMarketBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BitcoinMarketBox.Location = new System.Drawing.Point(612, 172);
+            this.BitcoinMarketBox.Name = "BitcoinMarketBox";
+            this.BitcoinMarketBox.Size = new System.Drawing.Size(130, 84);
+            this.BitcoinMarketBox.TabIndex = 7;
+            this.BitcoinMarketBox.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Developers);
+            this.groupBox1.Location = new System.Drawing.Point(7, 262);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(388, 153);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "StatusBox";
             // 
             // Placement_Board
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(235)))), ((int)(((byte)(208)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.CryptoMarket);
-            this.Controls.Add(this.WhiteBoard);
-            this.Controls.Add(this.BootCamp);
-            this.Controls.Add(this.Recruiting);
+            this.Controls.Add(this.BitcoinMarketBox);
+            this.Controls.Add(this.WhiteBoardBox);
+            this.Controls.Add(this.BootCampBox);
+            this.Controls.Add(this.HRBox);
+            this.Controls.Add(this.ProjectTiles);
+            this.Controls.Add(this.StatusBox);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Placement_Board";
             this.Text = "Placement_Board";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.StatusBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.holdDevelopers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Developers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HRBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BootCampBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WhiteBoardBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BitcoinMarketBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -103,9 +212,17 @@ namespace ScrumAge
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.ComboBox Recruiting;
-        private System.Windows.Forms.ComboBox BootCamp;
-        private System.Windows.Forms.ComboBox WhiteBoard;
-        private System.Windows.Forms.ComboBox CryptoMarket;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.GroupBox StatusBox;
+        private System.Windows.Forms.GroupBox ProjectTiles;
+        private System.Windows.Forms.PictureBox Developers;
+        private System.Windows.Forms.PictureBox HRBox;
+        private System.Windows.Forms.PictureBox BootCampBox;
+        private System.Windows.Forms.PictureBox WhiteBoardBox;
+        private System.Windows.Forms.PictureBox BitcoinMarketBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox holdDevelopers;
     }
 }
--- ScrumAge/Placement_Board.cs
+++ ScrumAge/Placement_Board.cs
@@ -23,29 +23,169 @@ namespace ScrumAge
 {
     public partial class Placement_Board : Form
     {
-        public Placement_Board()
+        PictureBox[] boxes;
+        PictureBox selected;
+        Player player;
+
+        public Placement_Board(Player player)
         {
             InitializeComponent();
+
+            boxes = new PictureBox[] { HRBox, BootCampBox, WhiteBoardBox, BitcoinMarketBox, holdDevelopers };
+            this.player = player;
+
+            try
+            {
+                //Supply your own images here
+                switch (player.Id)
+                {
+                    case 1:
+                        holdDevelopers.Image = Image.FromFile(@"Images\red.png");
+                        break;
+                    case 2:
+                        holdDevelopers.Image = Image.FromFile(@"Images\yellow.png");
+                        break;
+                    case 3:
+                        holdDevelopers.Image = Image.FromFile(@"Images\green.png");
+                        break;
+                    case 4:
+                        holdDevelopers.Image = Image.FromFile(@"Images\gray.png");
+                        break;
+                }
+
+                foreach (var box in boxes)
+                {
+                    box.AllowDrop = true;
+                    box.DragDrop += PictureBox_DragDrop;
+                    box.DragEnter += PictureBox_DragEnter;
+                    box.MouseClick += PictureBox_MouseClick;
+                    box.MouseMove += PictureBox_MouseMove;
+                    box.Paint += PictureBox_Paint;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No images found", "No Images Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
         }
 
-        private void Recruiting_SelectedIndexChanged(object sender, EventArgs e)
+        /// <summary>
+        /// Fires after dragging has completed on the target control
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void PictureBox_DragDrop(object sender, DragEventArgs e)
         {
+            var target = (PictureBox)sender;
+            if (e.Data.GetDataPresent(typeof(PictureBox)))
+            {
+                var source = (PictureBox)e.Data.GetData(typeof(PictureBox));
+                if (source != target)
+                {
+                    Console.WriteLine("Do DragDrop from " + source.Name + " to " + target.Name);
+                    // You can swap the images out, replace the target image, etc.
+                    SwapImages(source, target);
 
+                    selected = null;
+                    SelectBox(target);
+                    return;
+                }
+            }
+            Console.WriteLine("Don't do DragDrop");
         }
 
-        private void BootCamp_SelectedIndexChanged(object sender, EventArgs e)
+        /// <summary>
+        /// Set the target's accepted DragDropEffect. Should match the source.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void PictureBox_DragEnter(object sender, DragEventArgs e)
         {
-
+            e.Effect = DragDropEffects.Move;
         }
 
-        private void WhiteBoard_SelectedIndexChanged(object sender, EventArgs e)
+        /// <summary>
+        /// Handle mouse click on picture box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void PictureBox_MouseClick(object sender, MouseEventArgs e)
         {
-
+            SelectBox((PictureBox)sender);
         }
 
-        private void CryptoMarket_SelectedIndexChanged(object sender, EventArgs e)
+        /// <summary>
+        /// Only start DragDrop if the mouse moves. Allows MouseClick to trigger
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void PictureBox_MouseMove(object sender, MouseEventArgs e)
         {
+            if (e.Button == MouseButtons.Left)
+            {
+                var pb = (PictureBox)sender;
+                if (pb.BackgroundImage != null)
+                {
+                    pb.DoDragDrop(pb, DragDropEffects.Move);
+                }
+            }
+        }
 
+        /// <summary>
+        /// Override paint so we can draw a border on a selected image
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void PictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            var pb = (PictureBox)sender;
+            pb.BackColor = Color.White;
+            if (selected == pb)
+            {
+                ControlPaint.DrawBorder(e.Graphics, pb.ClientRectangle,
+                   Color.Blue, 5, ButtonBorderStyle.Solid,  // Left
+                   Color.Blue, 5, ButtonBorderStyle.Solid,  // Top
+                   Color.Blue, 5, ButtonBorderStyle.Solid,  // Right
+                   Color.Blue, 5, ButtonBorderStyle.Solid); // Bottom
+            }
+        }
+
+        /// <summary>
+        /// Set the selected image, and trigger repaint on all boxes.
+        /// </summary>
+        /// <param name="pb"></param>
+        void SelectBox(PictureBox pb)
+        {
+            if (selected != pb)
+            {
+                selected = pb;
+            }
+            else
+            {
+                selected = null;
+            }
+
+            // Cause each box to repaint
+            foreach (var box in boxes) box.Invalidate();
+        }
+
+        /// <summary>
+        /// Swap images between two PictureBoxes
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        void SwapImages(PictureBox source, PictureBox target)
+        {
+            if (source.BackgroundImage == null && target.BackgroundImage == null)
+            {
+                return;
+            }
+
+            var temp = target.BackgroundImage;
+            target.BackgroundImage = source.BackgroundImage;
+            source.BackgroundImage = temp;
         }
     }
 }
