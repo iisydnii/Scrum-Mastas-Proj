0893d8c9e2350411032b3645ff84b798c88781ff
Sydni Ward <61031342+iisydnii@users.noreply.github.com>
2021-10-11 12:53:52 -0400

    save

---
+++ ScrumAge/Developer_DragAndDrop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ScrumAge
+{
+    public partial class Developer_DragAndDrop : Placement_Board
+    {
+        public Developer_DragAndDrop()
+        {
+            InitializeComponent();
+
+            Developers.DragEnter += pictureBox5_DragEnter;
+            Developers.DragDrop += pictureBox5_DragDrop;
+        }
+
+        private void pictureBox5_DragDrop(object sender, DragEventArgs e)
+        {
+            var bmp = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+            pictureBox2.Image = bmp;
+        }
+
+        private void pictureBox5_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Bitmap))
+                e.Effect = DragDropEffects.Move;
+        }
+    }
+}
---
+++ ScrumAge/RoundButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Button;
+
+namespace ScrumAge
+{
+    class RoundButton : Button
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Call the OnPaint method of the base class.
+            base.OnPaint(e);
+
+            // Declare and instantiate a new pen that will be disposed of at the end of the method.
+            using var myPen = new Pen(Color.Aqua);
+
+            // Create a rectangle that represents the size of the control, minus 1 pixel.
+            var area = new Rectangle(new Point(0, 0), new Size(this.Size.Width - 1, this.Size.Height - 1));
+
+            // Draw an aqua rectangle in the rectangle represented by the control.
+            e.Graphics.DrawRectangle(myPen, area);
+        }
+    }
+}
