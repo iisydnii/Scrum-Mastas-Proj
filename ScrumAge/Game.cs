0a13b0b5ac5180c1f0f1487f94b78070d3dff0d3
trivetteag <trivetteag@etsu.edu>
2021-10-15 19:07:41 -0400

    Update to Game.CS

--- ScrumAge/Game.cs
+++ ScrumAge/Game.cs
@@ -1,7 +1,7 @@
 /*-------------
 Name: Austin Trivette
 Date created: 10/13/21
-Last Updated: 10/13/21
+Last Updated: 10/15/21
 File name: Game.cs
 Purpose: Control Game Logic
 -------------*/
@@ -13,18 +13,22 @@ using System.Collections.Generic;
 
 public static class Game
 {
-    // List of Players
     public static List<Player> PlayerList { get; set; }
 
     // Forms
-    public static SelectPlayersForm selectPlayersForm;
-    public static Placement_Board placement_Board;
+    private static LandingPageForm landingPageForm;
+    private static SelectPlayersForm selectPlayersForm;
+    private static Placement_Board placement_Board;
+    private static DiceForm diceForm;
+
 
     // Starts the game by displaying the Title Screen
     public static void StartGame()
     {
-        Application.Run(new LandingPageForm());    
+        landingPageForm = new LandingPageForm();
+        landingPageForm.ShowDialog();
     }
+
 
     public static void DisplaySetPlayersForm()
     {
@@ -37,6 +41,7 @@ public static class Game
         PopulatePlayers();
     }
 
+
     public static void DisplayPlacementBoard()
     {
         placement_Board = new Placement_Board();
@@ -44,20 +49,45 @@ public static class Game
         foreach (var player in PlayerList)
         {
             //placement_Board = new Placement_Board(player.ID);
+            //placement_Board.ShowDialog();
         }
         
     }
 
+
+    public static void DisplayDiceForm()
+    {
+        diceForm = new DiceForm();
+
+        diceForm.ShowDialog();
+    }
+    
+    ///TODO
     //public static void DisplayActivationBoard()
     //{
 
     //}
 
+    ///TODO
     //public static void DisplayRetrospective()
     //{
 
     //}
 
+    public static void CloseForm(Form form)
+    {
+        form.
+    }
+
+
+    // For Testing
+    public static void Test()
+    {
+        StartGame();
+        DisplaySetPlayersForm();
+        DisplayPlacementBoard();
+        DisplayDiceForm();
+    }
 
 
     private static void PopulatePlayers()
