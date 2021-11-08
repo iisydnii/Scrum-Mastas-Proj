19b7a623341790ce9dcc2dc1fae20bb8ffc6dc59
Sydni Ward <61031342+iisydnii@users.noreply.github.com>
2021-11-08 13:04:54 -0500

    update

--- ScrumAge/Player.cs
+++ ScrumAge/Player.cs
@@ -13,8 +13,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
-using System.Windows.Forms;
 
 namespace ScrumAge
 {
