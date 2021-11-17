72685789ce3a101d02b93a49ac729c64ec86704e
Sydni Ward <61031342+iisydnii@users.noreply.github.com>
2021-11-16 20:57:36 -0500

    Save small changes

--- ScrumAge/Retrospective.Designer.cs
+++ ScrumAge/Retrospective.Designer.cs
@@ -38,9 +38,9 @@ namespace ScrumAge
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.groupTheBad = new System.Windows.Forms.GroupBox();
             this.groupTheUgly = new System.Windows.Forms.GroupBox();
+            this.theUglyMessage = new System.Windows.Forms.TextBox();
             this.player = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.theUglyMessage = new System.Windows.Forms.TextBox();
             this.groupTheUgly.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,9 +48,10 @@ namespace ScrumAge
             // 
             this.next_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(124)))), ((int)(((byte)(116)))));
             this.next_button.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold);
-            this.next_button.Location = new System.Drawing.Point(667, 400);
+            this.next_button.Location = new System.Drawing.Point(1000, 615);
+            this.next_button.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.next_button.Name = "next_button";
-            this.next_button.Size = new System.Drawing.Size(121, 38);
+            this.next_button.Size = new System.Drawing.Size(182, 58);
             this.next_button.TabIndex = 0;
             this.next_button.Text = "Next";
             this.next_button.UseVisualStyleBackColor = false;
@@ -60,9 +61,10 @@ namespace ScrumAge
             this.theGood.AutoSize = true;
             this.theGood.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(124)))), ((int)(((byte)(116)))));
             this.theGood.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold);
-            this.theGood.Location = new System.Drawing.Point(157, 104);
+            this.theGood.Location = new System.Drawing.Point(236, 160);
+            this.theGood.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.theGood.Name = "theGood";
-            this.theGood.Size = new System.Drawing.Size(118, 25);
+            this.theGood.Size = new System.Drawing.Size(176, 38);
             this.theGood.TabIndex = 1;
             this.theGood.Text = "The Good";
             // 
@@ -71,9 +73,10 @@ namespace ScrumAge
             this.theBad.AutoSize = true;
             this.theBad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(158)))), ((int)(((byte)(70)))));
             this.theBad.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold);
-            this.theBad.Location = new System.Drawing.Point(344, 104);
+            this.theBad.Location = new System.Drawing.Point(516, 160);
+            this.theBad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.theBad.Name = "theBad";
-            this.theBad.Size = new System.Drawing.Size(105, 25);
+            this.theBad.Size = new System.Drawing.Size(156, 38);
             this.theBad.TabIndex = 2;
             this.theBad.Text = "The Bad";
             // 
@@ -82,18 +85,21 @@ namespace ScrumAge
             this.theUgly.AutoSize = true;
             this.theUgly.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(56)))), ((int)(((byte)(54)))));
             this.theUgly.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold);
-            this.theUgly.Location = new System.Drawing.Point(519, 104);
+            this.theUgly.Location = new System.Drawing.Point(778, 160);
+            this.theUgly.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.theUgly.Name = "theUgly";
-            this.theUgly.Size = new System.Drawing.Size(116, 25);
+            this.theUgly.Size = new System.Drawing.Size(173, 38);
             this.theUgly.TabIndex = 3;
             this.theUgly.Text = "The Ugly";
             // 
             // groupTheGood
             // 
             this.groupTheGood.BackColor = System.Drawing.Color.Transparent;
-            this.groupTheGood.Location = new System.Drawing.Point(143, 132);
+            this.groupTheGood.Location = new System.Drawing.Point(214, 203);
+            this.groupTheGood.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupTheGood.Name = "groupTheGood";
-            this.groupTheGood.Size = new System.Drawing.Size(147, 197);
+            this.groupTheGood.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupTheGood.Size = new System.Drawing.Size(220, 303);
             this.groupTheGood.TabIndex = 4;
             this.groupTheGood.TabStop = false;
             this.groupTheGood.Enter += new System.EventHandler(this.groupTheGood_Enter);
@@ -101,9 +107,11 @@ namespace ScrumAge
             // groupTheBad
             // 
             this.groupTheBad.BackColor = System.Drawing.Color.Transparent;
-            this.groupTheBad.Location = new System.Drawing.Point(325, 132);
+            this.groupTheBad.Location = new System.Drawing.Point(488, 203);
+            this.groupTheBad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupTheBad.Name = "groupTheBad";
-            this.groupTheBad.Size = new System.Drawing.Size(147, 197);
+            this.groupTheBad.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupTheBad.Size = new System.Drawing.Size(220, 303);
             this.groupTheBad.TabIndex = 5;
             this.groupTheBad.TabStop = false;
             this.groupTheBad.Enter += new System.EventHandler(this.groupTheBad_Enter);
@@ -112,21 +120,34 @@ namespace ScrumAge
             // 
             this.groupTheUgly.BackColor = System.Drawing.Color.Transparent;
             this.groupTheUgly.Controls.Add(this.theUglyMessage);
-            this.groupTheUgly.Location = new System.Drawing.Point(507, 132);
+            this.groupTheUgly.Location = new System.Drawing.Point(760, 203);
+            this.groupTheUgly.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupTheUgly.Name = "groupTheUgly";
-            this.groupTheUgly.Size = new System.Drawing.Size(147, 197);
+            this.groupTheUgly.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupTheUgly.Size = new System.Drawing.Size(220, 303);
             this.groupTheUgly.TabIndex = 6;
             this.groupTheUgly.TabStop = false;
             this.groupTheUgly.Enter += new System.EventHandler(this.groupTheUgly_Enter);
+            // 
+            // theUglyMessage
+            // 
+            this.theUglyMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(226)))), ((int)(((byte)(180)))));
+            this.theUglyMessage.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold);
+            this.theUglyMessage.Location = new System.Drawing.Point(26, 31);
+            this.theUglyMessage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.theUglyMessage.Name = "theUglyMessage";
+            this.theUglyMessage.Size = new System.Drawing.Size(148, 34);
+            this.theUglyMessage.TabIndex = 0;
             // 
             // player
             // 
             this.player.AutoSize = true;
             this.player.BackColor = System.Drawing.Color.Transparent;
             this.player.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold);
-            this.player.Location = new System.Drawing.Point(61, 51);
+            this.player.Location = new System.Drawing.Point(92, 78);
+            this.player.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(56, 22);
+            this.player.Size = new System.Drawing.Size(81, 32);
             this.player.TabIndex = 7;
             this.player.Text = "Player";
             // 
@@ -134,29 +155,21 @@ namespace ScrumAge
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F);
-            this.label1.Location = new System.Drawing.Point(123, 51);
+            this.label1.Location = new System.Drawing.Point(184, 78);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 22);
+            this.label1.Size = new System.Drawing.Size(75, 32);
             this.label1.TabIndex = 8;
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.playerName_Click);
             // 
-            // theUglyMessage
-            // 
-            this.theUglyMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(226)))), ((int)(((byte)(180)))));
-            this.theUglyMessage.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold);
-            this.theUglyMessage.Location = new System.Drawing.Point(17, 20);
-            this.theUglyMessage.Name = "theUglyMessage";
-            this.theUglyMessage.Size = new System.Drawing.Size(100, 25);
-            this.theUglyMessage.TabIndex = 0;
-            // 
             // Retrospective
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.player);
             this.Controls.Add(this.groupTheUgly);
@@ -167,7 +180,9 @@ namespace ScrumAge
             this.Controls.Add(this.theGood);
             this.Controls.Add(this.next_button);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Retrospective";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Retrospective";
             this.groupTheUgly.ResumeLayout(false);
             this.groupTheUgly.PerformLayout();
