0ce03d23bc22aec623f4e2b9299c0741344a424d
trivetteag <trivetteag@etsu.edu>
2021-12-01 18:43:14 -0500

    Made Player Label Bigger On status box

--- ScrumAge/Placement_Board.Designer.cs
+++ ScrumAge/Placement_Board.Designer.cs
@@ -309,7 +309,7 @@ namespace ScrumAge
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Palatino Linotype", 9.75F);
-            this.label6.Location = new System.Drawing.Point(57, 17);
+            this.label6.Location = new System.Drawing.Point(74, 19);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(48, 20);
             this.label6.TabIndex = 6;
@@ -348,10 +348,10 @@ namespace ScrumAge
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 11.78182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(5, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 21);
+            this.label1.Size = new System.Drawing.Size(73, 26);
             this.label1.TabIndex = 1;
             this.label1.Text = "Player:";
             // 
