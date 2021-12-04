7c10bbb7a5e43a4f8f15155f03ed6edb18fc8d44
Sydni Ward <61031342+iisydnii@users.noreply.github.com>
2021-12-03 22:29:16 -0500

    save

--- ScrumAge/Retrospective.Designer.cs
+++ ScrumAge/Retrospective.Designer.cs
@@ -38,10 +38,20 @@ namespace ScrumAge
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.groupTheBad = new System.Windows.Forms.GroupBox();
             this.groupTheUgly = new System.Windows.Forms.GroupBox();
-            this.theUglyMessage = new System.Windows.Forms.TextBox();
             this.player = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.groupTheUgly.SuspendLayout();
+            this.label11 = new System.Windows.Forms.Label();
+            this.labelDesign = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.labelBitcoin = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.labelDevelopment = new System.Windows.Forms.Label();
+            this.labelDevelopers = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labelTraining = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelCertifications = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // next_button
@@ -54,15 +64,16 @@ namespace ScrumAge
             this.next_button.TabIndex = 0;
             this.next_button.Text = "Next";
             this.next_button.UseVisualStyleBackColor = false;
+            this.next_button.Click += new System.EventHandler(this.next_button_Click);
             // 
             // theGood
             // 
             this.theGood.AutoSize = true;
             this.theGood.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(124)))), ((int)(((byte)(116)))));
             this.theGood.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold);
-            this.theGood.Location = new System.Drawing.Point(157, 104);
+            this.theGood.Location = new System.Drawing.Point(156, 104);
             this.theGood.Name = "theGood";
-            this.theGood.Size = new System.Drawing.Size(140, 30);
+            this.theGood.Size = new System.Drawing.Size(118, 25);
             this.theGood.TabIndex = 1;
             this.theGood.Text = "The Good";
             // 
@@ -71,9 +82,9 @@ namespace ScrumAge
             this.theBad.AutoSize = true;
             this.theBad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(158)))), ((int)(((byte)(70)))));
             this.theBad.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold);
-            this.theBad.Location = new System.Drawing.Point(344, 104);
+            this.theBad.Location = new System.Drawing.Point(347, 104);
             this.theBad.Name = "theBad";
-            this.theBad.Size = new System.Drawing.Size(123, 30);
+            this.theBad.Size = new System.Drawing.Size(105, 25);
             this.theBad.TabIndex = 2;
             this.theBad.Text = "The Bad";
             // 
@@ -82,9 +93,9 @@ namespace ScrumAge
             this.theUgly.AutoSize = true;
             this.theUgly.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(56)))), ((int)(((byte)(54)))));
             this.theUgly.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold);
-            this.theUgly.Location = new System.Drawing.Point(519, 104);
+            this.theUgly.Location = new System.Drawing.Point(522, 104);
             this.theUgly.Name = "theUgly";
-            this.theUgly.Size = new System.Drawing.Size(137, 30);
+            this.theUgly.Size = new System.Drawing.Size(116, 25);
             this.theUgly.TabIndex = 3;
             this.theUgly.Text = "The Ugly";
             // 
@@ -96,7 +107,6 @@ namespace ScrumAge
             this.groupTheGood.Size = new System.Drawing.Size(147, 197);
             this.groupTheGood.TabIndex = 4;
             this.groupTheGood.TabStop = false;
-            this.groupTheGood.Enter += new System.EventHandler(this.groupTheGood_Enter);
             // 
             // groupTheBad
             // 
@@ -106,49 +116,170 @@ namespace ScrumAge
             this.groupTheBad.Size = new System.Drawing.Size(147, 197);
             this.groupTheBad.TabIndex = 5;
             this.groupTheBad.TabStop = false;
-            this.groupTheBad.Enter += new System.EventHandler(this.groupTheBad_Enter);
             // 
             // groupTheUgly
             // 
             this.groupTheUgly.BackColor = System.Drawing.Color.Transparent;
-            this.groupTheUgly.Controls.Add(this.theUglyMessage);
             this.groupTheUgly.Location = new System.Drawing.Point(507, 132);
             this.groupTheUgly.Name = "groupTheUgly";
             this.groupTheUgly.Size = new System.Drawing.Size(147, 197);
             this.groupTheUgly.TabIndex = 6;
             this.groupTheUgly.TabStop = false;
-            this.groupTheUgly.Enter += new System.EventHandler(this.groupTheUgly_Enter);
-            // 
-            // theUglyMessage
-            // 
-            this.theUglyMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(226)))), ((int)(((byte)(180)))));
-            this.theUglyMessage.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold);
-            this.theUglyMessage.Location = new System.Drawing.Point(17, 20);
-            this.theUglyMessage.Name = "theUglyMessage";
-            this.theUglyMessage.Size = new System.Drawing.Size(100, 28);
-            this.theUglyMessage.TabIndex = 0;
             // 
             // player
             // 
             this.player.AutoSize = true;
             this.player.BackColor = System.Drawing.Color.Transparent;
             this.player.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold);
-            this.player.Location = new System.Drawing.Point(61, 51);
+            this.player.Location = new System.Drawing.Point(81, 23);
             this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(68, 26);
+            this.player.Size = new System.Drawing.Size(56, 22);
             this.player.TabIndex = 7;
             this.player.Text = "Player";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F);
-            this.label1.Location = new System.Drawing.Point(123, 51);
+            this.label1.Location = new System.Drawing.Point(137, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 26);
+            this.label1.Size = new System.Drawing.Size(50, 22);
             this.label1.TabIndex = 8;
             this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.playerName_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label11.Location = new System.Drawing.Point(201, 48);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(52, 18);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "Design";
+            // 
+            // labelDesign
+            // 
+            this.labelDesign.AutoSize = true;
+            this.labelDesign.BackColor = System.Drawing.Color.Transparent;
+            this.labelDesign.Font = new System.Drawing.Font("Palatino Linotype", 9.75F);
+            this.labelDesign.Location = new System.Drawing.Point(271, 48);
+            this.labelDesign.Name = "labelDesign";
+            this.labelDesign.Size = new System.Drawing.Size(51, 18);
+            this.labelDesign.TabIndex = 12;
+            this.labelDesign.Text = "label10";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label13.Location = new System.Drawing.Point(69, 71);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(51, 18);
+            this.label13.TabIndex = 15;
+            this.label13.Text = "Bitcoin";
+            // 
+            // labelBitcoin
+            // 
+            this.labelBitcoin.AutoSize = true;
+            this.labelBitcoin.BackColor = System.Drawing.Color.Transparent;
+            this.labelBitcoin.Font = new System.Drawing.Font("Palatino Linotype", 9.75F);
+            this.labelBitcoin.Location = new System.Drawing.Point(139, 71);
+            this.labelBitcoin.Name = "labelBitcoin";
+            this.labelBitcoin.Size = new System.Drawing.Size(51, 18);
+            this.labelBitcoin.TabIndex = 14;
+            this.labelBitcoin.Text = "label12";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.BackColor = System.Drawing.Color.Transparent;
+            this.label14.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label14.Location = new System.Drawing.Point(201, 71);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(91, 18);
+            this.label14.TabIndex = 19;
+            this.label14.Text = "Development";
+            // 
+            // labelDevelopment
+            // 
+            this.labelDevelopment.AutoSize = true;
+            this.labelDevelopment.BackColor = System.Drawing.Color.Transparent;
+            this.labelDevelopment.Font = new System.Drawing.Font("Palatino Linotype", 9.75F);
+            this.labelDevelopment.Location = new System.Drawing.Point(297, 71);
+            this.labelDevelopment.Name = "labelDevelopment";
+            this.labelDevelopment.Size = new System.Drawing.Size(44, 18);
+            this.labelDevelopment.TabIndex = 18;
+            this.labelDevelopment.Text = "label3";
+            // 
+            // labelDevelopers
+            // 
+            this.labelDevelopers.AutoSize = true;
+            this.labelDevelopers.BackColor = System.Drawing.Color.Transparent;
+            this.labelDevelopers.Font = new System.Drawing.Font("Palatino Linotype", 9.75F);
+            this.labelDevelopers.Location = new System.Drawing.Point(461, 48);
+            this.labelDevelopers.Name = "labelDevelopers";
+            this.labelDevelopers.Size = new System.Drawing.Size(44, 18);
+            this.labelDevelopers.TabIndex = 21;
+            this.labelDevelopers.Text = "label7";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(359, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 18);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Developers";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label9.Location = new System.Drawing.Point(65, 48);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 18);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Training";
+            // 
+            // labelTraining
+            // 
+            this.labelTraining.AutoSize = true;
+            this.labelTraining.BackColor = System.Drawing.Color.Transparent;
+            this.labelTraining.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTraining.Location = new System.Drawing.Point(139, 48);
+            this.labelTraining.Name = "labelTraining";
+            this.labelTraining.Size = new System.Drawing.Size(44, 18);
+            this.labelTraining.TabIndex = 22;
+            this.labelTraining.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(359, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 18);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Certifications";
+            // 
+            // labelCertifications
+            // 
+            this.labelCertifications.AutoSize = true;
+            this.labelCertifications.BackColor = System.Drawing.Color.Transparent;
+            this.labelCertifications.Font = new System.Drawing.Font("Palatino Linotype", 9.75F);
+            this.labelCertifications.Location = new System.Drawing.Point(472, 71);
+            this.labelCertifications.Name = "labelCertifications";
+            this.labelCertifications.Size = new System.Drawing.Size(44, 18);
+            this.labelCertifications.TabIndex = 25;
+            this.labelCertifications.Text = "label5";
+            this.labelCertifications.Click += new System.EventHandler(this.labelCertifications_Click);
             // 
             // Retrospective
             // 
@@ -157,6 +288,18 @@ namespace ScrumAge
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelCertifications);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.labelTraining);
+            this.Controls.Add(this.labelDevelopers);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.labelDevelopment);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.labelBitcoin);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.labelDesign);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.player);
             this.Controls.Add(this.groupTheUgly);
@@ -171,8 +314,7 @@ namespace ScrumAge
             this.Name = "Retrospective";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Retrospective";
-            this.groupTheUgly.ResumeLayout(false);
-            this.groupTheUgly.PerformLayout();
+            this.Load += new System.EventHandler(this.Retrospective_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -190,6 +332,17 @@ namespace ScrumAge
         private System.Windows.Forms.GroupBox groupTheUgly;
         private System.Windows.Forms.Label player;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox theUglyMessage;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label labelDesign;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label labelBitcoin;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label labelDevelopment;
+        private System.Windows.Forms.Label labelDevelopers;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labelTraining;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelCertifications;
     }
 }
